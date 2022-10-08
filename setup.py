642028384c6c8794f5f8e59619dc58722a9d8a3f
Audrey Beard <audrey.s.beard@gmail.com>
2022-10-08 13:07:14 -0500

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='compressure',
+    version='0.0',
+    description='compression artifacts',
+    author='mip',
+    url='github.com/AudreyBeard/compressure',
+    packages=['compressure'],
+)
