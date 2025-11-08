c65986ac8576e05f3aa796b6a31f17eeb1a2049c
Audrey Beard <audrey.s.beard@gmail.com>
2025-11-07 18:50:08 -0600

    updates

--- compressure/main_v2.py
+++ compressure/main_v2.py
@@ -1,11 +1,16 @@
+import cProfile
 import logging
 import queue
 import subprocess
 import threading
 from pathlib import Path
-from typing import List
+from typing import (
+    List,
+    Tuple,
+)
 
 import mido
+from tqdm import tqdm
 
 from compressure.compression import SingleVideoCompression
 from compressure.midi import ParserDeque  # noqa
@@ -72,6 +77,30 @@ def get_video_metadata(
     return metadata
 
 
+def extract_clip(
+    fpath_in: str,
+    t_start: float,
+    t_end: float,
+    fpath_out: str,
+):
+    cmd = [
+        "ffmpeg", "-y",
+        "-copyts",
+        "-ss", f"{t_start:.3f}",
+        "-i", fpath_in,
+        "-to", f"{t_end:.3f}",
+        "-map", "0",
+        "-c", "copy",
+        fpath_out,
+    ]
+    try:
+        result = subprocess.run(cmd, capture_output=True, check=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        logger.info(f"[ffmpeg error] {e.stderr.decode().strip()}")
+        raise ValueError(e.stderr.decode().strip())
+
+
 def reverse_video(
     fpath_in: str,
     fpath_out: str,
@@ -101,11 +130,12 @@ def import_reverse(
     qp: int = -1,
     preset: str = "ultrafast",
     gop_size: int = 6000,
+    workdir: str = "~/.cache/compressure",
 ):
     fpath_in_ = Path(fpath_in).expanduser().absolute()
     fpath_reverse_ = fpath_in_.with_stem(fpath_in_.stem + "_reverse").with_suffix(".mp4")
 
-    print("Reversing original video")
+    #print("Reversing original video")
     reverse_video(
         fpath_in=str(fpath_in_),
         fpath_out=str(fpath_reverse_),
@@ -115,7 +145,7 @@ def import_reverse(
 
     compressor = SingleVideoCompression(
         fpath_in=str(fpath_in_),
-        workdir=str(Path('.').absolute()),
+        workdir=str(Path(workdir).expanduser().absolute()),
         gop_size=gop_size,
         encoder='libx264',
         encoder_config={
@@ -123,12 +153,12 @@ def import_reverse(
             'preset': preset
         },
     )
-    print("Compressing input")
+    #print("Compressing input")
     fpath_out, _ = compressor.transcode_video()
 
     compressor = SingleVideoCompression(
         fpath_in=str(fpath_reverse_),
-        workdir=str(Path('.').absolute()),
+        workdir=str(Path(workdir).expanduser().absolute()),
         gop_size=gop_size,
         encoder='libx264',
         encoder_config={
@@ -137,7 +167,7 @@ def import_reverse(
         },
     )
 
-    print("Compressing reversed")
+    #print("Compressing reversed")
     fpath_out_reverse, _ = compressor.transcode_video()
     return fpath_out, fpath_out_reverse
 
@@ -179,7 +209,7 @@ class MultiNavigationController(object):
         self.fpaths_fwd = fpaths_fwd
         self.fpaths_bak = fpaths_bak
         self.controllers = []
-        for i, (fp_fwd, fp_bak) in enumerate(fpaths_fwd, fpaths_bak):
+        for i, (fp_fwd, fp_bak) in enumerate(zip(fpaths_fwd, fpaths_bak)):
             self.controllers.append(NavigationController(
                 fp_fwd,
                 fp_bak
@@ -201,19 +231,60 @@ class MultiNavigationController(object):
         width: float,
         index: int = -1,
     ):
-        self.index = index if index >= 0 else self.index
+        self.index = index if index is not None and index >= 0 else self.index
         return self.controllers[index].navigate(position, width)
+
+
+def import_chop_video(
+    fpath_in: str,
+    n_pieces: int = 8,
+    qp: int = -1,
+    preset: str = "ultrafast",
+    gop_size: int = 6000,
+    workdir: str = "~/.cache/compressure",
+) -> Tuple[List[str], List[str]]:
+    fpath_in_ = Path(fpath_in).expanduser().absolute()
+    md = get_video_metadata(str(fpath_in_))
+
+    fpaths_out = [None for i in range(n_pieces)]
+    fpaths_out_rev = [None for i in range(n_pieces)]
+
+    for i in tqdm(range(n_pieces), desc="Splitting & processing input video"):
+        t_start = i * md['duration'] / n_pieces
+        t_end = (i + 1) * md['duration'] / n_pieces
+        fname_out = fpath_in_.stem + str(i) + ".avi"
+        fpath_out = Path(workdir).expanduser().absolute() / fname_out
+        extract_clip(
+            str(fpath_in_),
+            t_start,
+            t_end,
+            str(fpath_out)
+        )
+        fpaths_out[i], fpaths_out_rev[i] = import_reverse(
+            str(fpath_out)
+        )
+
+    return fpaths_out, fpaths_out_rev
 
 
 def main(
     fpath_in: str,
     midi_object_name: str = 'nanoKONTROL2 SLIDER/KNOB',
     width_max: float = 1.0,
+    split_clip: bool = False,
 ):
 
-    fpath_fwd, fpath_bak = import_reverse(
-        fpath_in=fpath_in,
-    )
+    # TODO finish up the split clip functionality
+    if split_clip:
+        fpaths_fwd, fpaths_bak = import_chop_video(
+            fpath_in=fpath_in,
+        )
+        fpath_fwd = fpaths_fwd[0]
+        fpath_bak = fpaths_bak[0]
+    else:
+        fpath_fwd, fpath_bak = import_reverse(
+            fpath_in=fpath_in,
+        )
 
     print("getting metadata")
     md = get_video_metadata(
@@ -221,10 +292,16 @@ def main(
     )
 
     print("initializing navigator")
-    navigator = NavigationController(
-        fpath_fwd,
-        fpath_bak,
-    )
+    if split_clip:
+        navigator = MultiNavigationController(
+            fpaths_fwd,
+            fpaths_bak,
+        )
+    else:
+        navigator = NavigationController(
+            fpath_fwd,
+            fpath_bak,
+        )
 
     chunk_q = queue.Queue(maxsize=500)
 
@@ -249,6 +326,7 @@ def main(
 
     position = 0.0
     width = 0.5
+    index = 0
     fpath_selected = fpath_fwd
     with mido.open_input(midi_object_name) as port:
         #port._queue = ParserDeque()
@@ -274,16 +352,28 @@ def main(
             #if msg_old:
             msg = get_midi_message(port)
             if msg:
-                position, width = update_position_width(
+                if msg.control == 64:
+                    position = 0
+                    fpath_selected = fpath_fwd
+                    navigator.selected_stream = fpath_selected
+                    navigator.pos_current = position
+
+                position, width, index = update_position_width_index(
                     midi_msg=msg,
                     position_old=position,
                     width_old=width,
+                    index_old=index,
                     video_duration=md['duration'],
                     video_frames=md['frames']
                 )
 
-                fpath_selected, _, _ = navigator.navigate(position, width)
-                position = md['duration'] - position if fpath_selected == fpath_bak else position
+                if split_clip:
+                    fpath_selected, _, _ = navigator.navigate(position, width, index)
+                    position = md['duration'] - position if fpath_selected in fpaths_bak else position
+                else:
+                    fpath_selected, _, _ = navigator.navigate(position, width)
+                    position = md['duration'] - position if fpath_selected == fpath_bak else position
+
                 print(fpath_selected, f"{position:.3f}", f"{width:.3f}")
                 producer.update(
                     fpath_video=fpath_selected,
@@ -292,34 +382,59 @@ def main(
                 )
 
 
-def update_position_width(
+def update_position_width_index(
     midi_msg,
     position_old: float,
     width_old: float,
+    index_old: int,
     video_duration: float,
     video_frames: int,
 ):
-    position, width = position_old, width_old
+    def is_position(cc):
+        return cc >= 120 and cc <= 127
+
+    def is_width(cc):
+        return cc >= 16 and cc <= 23
+
+    def get_index(cc):
+        if is_position(cc):
+            return cc % 120
+        elif is_width(cc):
+            return cc % 16
+        else:
+            return cc % 64
+
+    position, width, index = position_old, width_old, index_old
     try:
-        if midi_msg.control == 120:
+        if is_position(midi_msg.control):
             position = midi_msg.value / 127 * video_duration
-        elif midi_msg.control == 16:
+        elif is_width(midi_msg.control):
             width = midi_msg.value / (video_frames / video_duration)
     except AttributeError:
         pass
 
-    return position, width
+    index = get_index(midi_msg.control)
+
+    return position, width, index
 
 
 def get_midi_message(
     port,
 ):
+    """ This is a hacky and inefficient way to get and deliver only the most
+        recent message in the queue
+    """
     msg_sample = port.poll()
     msg = msg_sample
     while msg_sample is not None:
         msg = msg_sample
         msg_sample = port.poll()
     return msg
+
+
+def profile(
+):
+    cProfile.run('from compressure.main_v2 import main; main("input.mp4")')
 
 
 if __name__ == "__main__":
